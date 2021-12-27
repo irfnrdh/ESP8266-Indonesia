15b6c54027d66792ea746c173a5cd11409dd3146
irfnrdh <kang.fand@gmail.com>
2021-12-27 22:42:15 +0700

    LED Bawaan Dak-dik-duk (Pertama)

--- src/main.cpp
+++ src/main.cpp
@@ -1,157 +1,21 @@
 // ESP8266 Indonesia
 // oleh @irfnrdh
-// Rilis 0.2
-// Web Server Kontrol LED Bawaan dengan Notif via telegram
+// Rilis 0.3
+// LED Bawaan Dak-dik-duk (Pertama)
 
-// Load Wi-Fi library
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <UniversalTelegramBot.h> 
-#include <ArduinoJson.h>
-
-
-// Replace with your network credentials
-const char* ssid     = "";
-const char* password = "";
-
-// Initialize Telegram BOT
-#define BOTtoken "" 
-#define CHAT_ID ""
-
-X509List cert(TELEGRAM_CERTIFICATE_ROOT);
-WiFiClientSecure client;
-UniversalTelegramBot bot(BOTtoken, client);
-
-// Set web server port number to 80
-WiFiServer server(80);
-
-// Variable to store the HTTP request
-String header;
-
-// Auxiliar variables to store the current output state
-String LEDState = "off";
-
-// Assign output variables to GPIO pins
-#define LED_BAWAAN D4
-
-// Current time
-unsigned long currentTime = millis();
-// Previous time
-unsigned long previousTime = 0; 
-// Define timeout time in milliseconds (example: 2000ms = 2s)
-const long timeoutTime = 2000;
 
 void setup() {
+  // letak output aliran
+  pinMode(D4,OUTPUT);
   Serial.begin(9600);
-  configTime(0, 0, "pool.ntp.org");      // get UTC time via NTP
-  client.setTrustAnchors(&cert); // Add root certificate for api.telegram.org
-
-
-  // Initialize the output variables as outputs
-  pinMode(LED_BAWAAN, OUTPUT);
-  // Set outputs to LOW
-  digitalWrite(LED_BAWAAN, LOW);
-
-  // Connect to Wi-Fi network with SSID and password
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  // Print local IP address and start web server
-  Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  server.begin();
-
-  bot.sendMessage(CHAT_ID, "EHESPE dimulai", "");
-  bot.sendMessage(CHAT_ID, "busyet dah", "");
+  Serial.println();
+  Serial.print("Mulai berjalan!");
 }
 
-void loop(){
-  WiFiClient client = server.available();   // Listen for incoming clients
-
-  if (client) {                             // If a new client connects,
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    currentTime = millis();
-    previousTime = currentTime;
-    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
-      currentTime = millis();         
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /led/on") >= 0) {
-              Serial.println("LED on");
-              LEDState = "on";
-              digitalWrite(LED_BAWAAN, HIGH);
-              bot.sendMessage(CHAT_ID, "Lampu LED dimatikan!", "");
-            } else if (header.indexOf("GET /led/off") >= 0) {
-              Serial.println("LED off");
-              LEDState = "off";
-              digitalWrite(LED_BAWAAN, LOW);
-              bot.sendMessage(CHAT_ID, "Lampu LED dihidupkan!", "");
-            } 
-            
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons 
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #77878A;}</style></head>");
-            
-            // Web Page Heading
-            client.println("<body><h1>ESP8266 Web Server</h1>");
-            
-            // Display current state, and ON/OFF buttons for GPIO 5  
-            client.println("<p>Led Bawaan - State " + LEDState + "</p>");
-            // If the LEDState is off, it displays the ON button       
-            if (LEDState=="off") {
-              client.println("<p><a href=\"/led/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/led/off\"><button class=\"button button2\">OFF</button></a></p>");
-            } 
-               
-     
-            client.println("</body></html>");
-            
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-      }
-    }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
-  }
+void loop() {
+  digitalWrite(D4, HIGH); //dakk
+  delay(1000); // dukk
+  digitalWrite(D4, LOW); //dikk
+  delay(1000); // dukk
 }
