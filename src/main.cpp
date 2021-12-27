27cc2412cda0145de2ceb160125a8fcecfd9a00b
irfnrdh <kang.fand@gmail.com>
2021-12-27 22:46:14 +0700

    Konek Wi-fi dengan indikator LED (Kedua)

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,44 @@
 // ESP8266 Indonesia
 // oleh @irfnrdh
-// Rilis 0.3
-// LED Bawaan Dak-dik-duk (Pertama)
+// Rilis 0.4
+// Konek Wi-fi dengan indikator LED (Kedua)
 
-#include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+const char* ssid="Manye name wifi engko?";
+const char* password = "passw0rdnye manye?";
+
 
 void setup() {
-  // letak output aliran
-  pinMode(D4,OUTPUT);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN,LOW);
+
   Serial.begin(9600);
   Serial.println();
-  Serial.print("Mulai berjalan!");
+  Serial.print("Wifi nak konek ne ");
+  Serial.println( ssid );
+
+  WiFi.begin(ssid,password);
+
+  Serial.println();
+  Serial.print("Tengah konek ne");
+
+  while( WiFi.status() != WL_CONNECTED ){
+      delay(500);
+      Serial.print(".");        
+  }
+
+  digitalWrite( LED_BUILTIN , HIGH);
+  Serial.println();
+
+  Serial.println("Kaba Mende, Wifi dah konek!");
+  Serial.print("Alamat engko ne : ");
+  Serial.println(WiFi.localIP() );
+
 }
 
 void loop() {
-  digitalWrite(D4, HIGH); //dakk
-  delay(1000); // dukk
-  digitalWrite(D4, LOW); //dikk
-  delay(1000); // dukk
+  // cadek usah ko pike ke kali ne
+
 }
